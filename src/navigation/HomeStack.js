bd03b50ae43d808c29010a06a6abf0addc6d1589
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-12-04 15:07:07 -0500

    Apply formatter

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import CreateChannelScreen from '../screens/CreateChannelScreen';
 import { IconButton } from 'react-native-paper';
-import ChannelScreen from '../screens/ChannelScreen';
+import ChatScreen from '../screens/ChatScreen';
 import BrowseChannelsScreen from '../screens/BrowseChannelsScreen';
 
 const ChatStack = createStackNavigator();
@@ -11,61 +11,61 @@ const ModalStack = createStackNavigator();
 
 export default function HomeStack() {
   return (
-      <ModalStack.Navigator mode="modal" headerMode="none">
-        <ModalStack.Screen name="ChatApp" component={ChatComponent} />
-        <ModalStack.Screen name="CreateChannel" component={CreateChannelScreen} />
-      </ModalStack.Navigator>
+    <ModalStack.Navigator mode="modal" headerMode="none">
+      <ModalStack.Screen name="ChatApp" component={ChatComponent} />
+      <ModalStack.Screen name="CreateChannel" component={CreateChannelScreen} />
+    </ModalStack.Navigator>
   );
 }
 
 function ChatComponent() {
   return (
-      <ChatStack.Navigator
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#5b3a70',
-            },
-            headerTintColor: '#ffffff',
-            headerTitleStyle: {
-              fontSize: 22,
-            },
-          }}
-      >
-        <ChatStack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={({ navigation }) => ({
-              headerRight: () => (
-                  <IconButton
-                      icon="plus"
-                      size={28}
-                      color="#ffffff"
-                      onPress={() => navigation.navigate('BrowseChannels')}
-                  />
-              ),
-            })}
-        />
-        <ChatStack.Screen
-            name="BrowseChannels"
-            component={BrowseChannelsScreen}
-            options={({ navigation }) => ({
-              headerRight: () => (
-                  <IconButton
-                      icon="plus"
-                      size={28}
-                      color="#ffffff"
-                      onPress={() => navigation.navigate('CreateChannel')}
-                  />
-              ),
-            })}
-        />
-        <ChatStack.Screen
-            name="Channel"
-            component={ChannelScreen}
-            options={({ route }) => ({
-              title: route.params.channel.name
-            })}
-        />
-      </ChatStack.Navigator>
+    <ChatStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#5b3a70',
+        },
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          fontSize: 22,
+        },
+      }}
+    >
+      <ChatStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <IconButton
+              icon="plus"
+              size={28}
+              color="#ffffff"
+              onPress={() => navigation.navigate('BrowseChannels')}
+            />
+          ),
+        })}
+      />
+      <ChatStack.Screen
+        name="BrowseChannels"
+        component={BrowseChannelsScreen}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <IconButton
+              icon="plus"
+              size={28}
+              color="#ffffff"
+              onPress={() => navigation.navigate('CreateChannel')}
+            />
+          ),
+        })}
+      />
+      <ChatStack.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={({ route }) => ({
+          title: route.params.channel.name,
+        })}
+      />
+    </ChatStack.Navigator>
   );
 }
